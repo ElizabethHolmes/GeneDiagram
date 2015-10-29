b387d7924971fef37fe21daabb7e5d251080149e
Elizabeth Sutton <elizabeth.r.sutton@gmail.com>
2015-10-29 17:39:44 +0000

    Update Gene.java

--- src/Gene.java
+++ src/Gene.java
@@ -1,11 +1,12 @@
 package genediagram;
 
+//Class for storing gene attributes
 public class Gene {
     
     public Gene (String[] details) {
         name = details[0];
         category = details[1];
-        direction = (details[2].equals("+")) ? true : false;
+        direction = (details[2].equals("+"));
         start = Integer.parseInt(details[3]);
         stop = Integer.parseInt(details[4]);
     }
